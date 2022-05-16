e7ce2900f2c09d2e96346acd7857c44608c88b15
Choi <EGchoi.dev@gmail.com>
2022-05-16 17:23:49 +0900

    레이아웃 작성중

--- app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
@@ -2,10 +2,16 @@ package fastcampus.aop.part3.chapter04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fastcampus.aop.part3.chapter04.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
