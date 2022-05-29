b832b693bac7294ef9ded753620241f0f8d2929f
moonggae <email@email.com>
2022-05-29 21:03:57 +0900

    appbar layout

--- app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
@@ -32,7 +32,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        binding.gatheringDigitalThingsLayout.setTransitionListener(object : MotionLayout.TransitionListener{
+        binding.gatheringDigitalThingsLayout.setTransitionListener(object :
+            MotionLayout.TransitionListener {
             override fun onTransitionStarted(
                 motionLayout: MotionLayout?,
                 startId: Int,
@@ -40,21 +41,26 @@ class MainActivity : AppCompatActivity() {
             ) {
                 isGatheringMotionAnimating = true
             }
+
             override fun onTransitionChange(
                 motionLayout: MotionLayout?,
                 startId: Int,
                 endId: Int,
                 progress: Float
-            ) {}
+            ) {
+            }
+
             override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
                 isGatheringMotionAnimating = false
             }
+
             override fun onTransitionTrigger(
                 motionLayout: MotionLayout?,
                 triggerId: Int,
                 positive: Boolean,
                 progress: Float
-            ) {}
+            ) {
+            }
         })
     }
 }
